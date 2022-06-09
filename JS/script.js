4acb733f20a788b6651375f0fd8bd96a738edaee
elzneik <104824463+elzneik@users.noreply.github.com>
2022-06-09 18:43:27 +0200

    add code to record the items

--- JS/script.js
+++ JS/script.js
@@ -23,12 +23,20 @@ function newItem () {
         li.toggleClass(strike);    // Question: I do not understand that line
     };
 
-    // 3. adding the delete button (x)
+    // 3.1 adding the delete button (x)
     // let crossOutButton = $("crossOutButton");
     let crossOutButton = $("<crossOutButton> </crossOutButton>"); // Question: What is the difference between line 27 and 28
    crossOutButton.append(document.createTextNode('X'));
     li.append(crossOutButton);
 
     // $("crossOutButton").on("click").delete("li");
-    crossOutButton.on("click", deleteListItem);
+    crossOutButton.on("click", deleteListItem);  // Question: What is the difference to line 32 
+
+    // 3.2 Adding class delete (display none) from the css
+    function deleteListItem () {
+        li.classList("delete");
+    }
+
+    // 4. Reordering the items
+    $("list").sortable();
 }
