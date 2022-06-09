cbfa5b4518ca078905d9897342dc644828727779
elzneik <104824463+elzneik@users.noreply.github.com>
2022-06-09 17:47:56 +0200

    correct code for variable li

--- JS/script.js
+++ JS/script.js
@@ -3,7 +3,7 @@ function newItem () { // set function
     // jQuery
     // 1. Adding a new item to the list of items
 
-    let li = $(".container").appnend("<li> </li>");
+    let li = $("<li> </li>");
     let inputValue = $(#input).val();
     let text = $(" ");
     li.append("inputValue");
